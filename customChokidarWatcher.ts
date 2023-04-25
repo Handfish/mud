9161381332545a751102aab8a727526802d66502
Kenneth Udovic <kennyudovic@gmail.com>
2023-04-25 17:42:49 -0400

    chore: @kudovs/turbowatch modification
    chore: decent demo
    
    chore: craziness
    
    chore: test
    
    chore: tagged template literal callback
    
    chore: delete extra file
    
    chore: bump version

---
+++ customChokidarWatcher.ts
@@ -0,0 +1,114 @@
+import { FileWatchingBackend } from "@kudovs/turbowatch";
+import { Roarr } from "roarr";
+import * as chokidar from "chokidar";
+
+const Logger = Roarr.child({
+  package: "turbowatch",
+});
+
+const log = Logger.child({
+  namespace: "CustomChokidarWatcher",
+});
+
+export class CustomChokidarWatcher extends FileWatchingBackend {
+  private chokidar: chokidar.FSWatcher;
+
+  private indexingIntervalId: NodeJS.Timeout;
+
+  public constructor(project: string) {
+    super();
+
+    let discoveredFileCount = 0;
+
+    this.indexingIntervalId = setInterval(() => {
+      log.trace(
+        "indexed %s %s...",
+        discoveredFileCount.toLocaleString("en-US"),
+        discoveredFileCount === 1 ? "file" : "files"
+      );
+    }, 5_000);
+
+    this.chokidar = chokidar.watch(project, {
+      awaitWriteFinish: false,
+      followSymlinks: true,
+      ignored: [
+        "node_modules",
+        ".turbo",
+        "dist",
+        "out",
+        "abi",
+        "types",
+        "build",
+        "bin",
+        "protoc",
+        "protobuf",
+        "src/mud-definitions",
+        ".parcel-cache",
+
+        "node_modules/**",
+        ".turbo/**",
+        "dist/**",
+        "out/**",
+        "abi/**",
+        "types/**",
+        "build/**",
+        "bin/**",
+        "protoc/**",
+        "protobuf/**",
+        "src/mud-definitions/**",
+        ".parcel-cache/**",
+
+        "**/node_modules/**",
+        "**/.turbo/**",
+        "**/dist/**",
+        "**/out/**",
+        "**/abi/**",
+        "**/types/**",
+        "**/build/**",
+        "**/bin/**",
+        "**/protoc/**",
+        "**/protobuf/**",
+        "**/src/mud-definitions/**",
+        "**/.parcel-cache/**",
+      ],
+    });
+
+    let ready = false;
+
+    this.chokidar.on("ready", () => {
+      clearInterval(this.indexingIntervalId);
+
+      ready = true;
+
+      this.emitReady();
+    });
+
+    this.chokidar.on("all", (event, filename) => {
+      if (!ready) {
+        discoveredFileCount++;
+
+        return;
+      }
+
+      if (event === "addDir") {
+        return;
+      }
+
+      this.emitChange({ filename });
+    });
+  }
+
+  public add(path: string) {
+    this.chokidar.add(path);
+  }
+
+  public unwatch(path: string) {
+    this.chokidar.unwatch(path);
+  }
+
+  public close() {
+    clearInterval(this.indexingIntervalId);
+
+    return this.chokidar.close();
+  }
+}
---
+++ turbowatch.ts
@@ -0,0 +1,71 @@
+import { ChangeEvent, watch } from "@kudovs/turbowatch";
+import { CustomChokidarWatcher } from "./customChokidarWatcher";
+import { getWorkspaceRoot } from "workspace-tools";
+
+const cwd = process.cwd();
+const workspaceRoot = getWorkspaceRoot(cwd);
+const start = Date.now();
+const unwatchArray: string[] = [];
+
+watch({
+  project: workspaceRoot!,
+  Watcher: CustomChokidarWatcher,
+  triggers: [
+    {
+      expression: [
+        "allof",
+        [
+          "allof",
+          ["not", ["dirname", "node_modules"]],
+          ["not", ["dirname", ".turbo"]],
+          ["not", ["dirname", "dist"]],
+          ["not", ["dirname", "out"]],
+          ["not", ["dirname", "abi"]],
+          ["not", ["dirname", "types"]],
+          ["not", ["dirname", "build"]],
+          ["not", ["dirname", "bin"]],
+          ["not", ["dirname", "protoc"]],
+          ["not", ["dirname", "protobuf"]],
+          ["not", ["dirname", "src/mud-definitions"]],
+          ["not", ["dirname", ".parcel-cache"]],
+        ],
+        // ["anyof", ...depPaths.map((dir) => ["dirname", dir] satisfies Expression)],
+        // ["anyof", ["dirname", "packages/store"]],
+        ["anyof", ["match", "*.sol", "basename"]],
+      ],
+      name: `turbowatch_sol`,
+      interruptible: true,
+      initialRun: true,
+      persistent: true,
+      onChange: async ({ spawn, files, first, abortSignal, watcher }: ChangeEvent) => {
+        if (first) {
+          console.log("Turbowatch started in", Date.now() - start, "ms");
+        } else {
+          console.log(
+            "File change detected",
+            files.map((f) => f.name)
+          );
+        }
+        console.log("Building workspace dependencies");
+
+        const filenameTokens = files[0].name.split("/");
+        const index = filenameTokens.lastIndexOf("packages");
+        const changedWorkspace = filenameTokens[index + 1];
+
+        const packagePath = filenameTokens.slice(0, index + 2).join("/");
+        console.log(packagePath);
+
+        watcher?.unwatch?.(packagePath);
+        unwatchArray.push(packagePath);
+        console.log("unwatch " + packagePath);
+
+        await spawn(() => {
+          watcher?.add?.(packagePath);
+        })`turbo build --filter=${changedWorkspace}`;
+        watcher?.add?.(packagePath);
+
+        if (abortSignal?.aborted) return;
+      },
+    },
+  ],
+});
