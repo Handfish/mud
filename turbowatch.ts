e569092178866c46b6e1a180ebae3d47522ea714
Kenneth Udovic <kennyudovic@gmail.com>
2023-04-30 10:18:40 -0400

    chore: test

--- turbowatch.ts
+++ turbowatch.ts
@@ -1,56 +1,10 @@
-import path from "path";
-import { ChangeEvent, Expression, watch } from "turbowatch";
-// import { ChokidarWatcher } from "./chokidarWatcher";
-import { CustomChokidarWatcher } from "turbowatch";
-import { getPackageInfos, createPackageGraph, getWorkspaceRoot, PackageGraph } from "workspace-tools";
+import { ChangeEvent, watch } from "@kudovs/turbowatch";
+import { CustomChokidarWatcher } from "./customChokidarWatcher";
+import { getWorkspaceRoot } from "workspace-tools";
 
 const cwd = process.cwd();
-// console.log(cwd);
-
 const workspaceRoot = getWorkspaceRoot(cwd);
-
-/*
-const packageInfos = getPackageInfos(workspaceRoot!);
-// const targetWorkspace = process.argv[2];
-const targetWorkspace = "@latticexyz/cli";
-
-if (!targetWorkspace || !(targetWorkspace in packageInfos)) {
-  throw new Error(
-    `${targetWorkspace}" is not a valid workspace. Must pass a valid workspace name as it appears in package.json.`
-  );
-}
-
-// Gets all direct and transitive dependencies for a workspace
-const getAllDependencies = (target: string, graph: PackageGraph) => {
-  return Array.from(
-    graph.dependencies.reduce((acc, dep) => {
-      if (dep.name === target) {
-        acc.add(dep.dependency);
-        getAllDependencies(dep.dependency, graph).forEach((val) => acc.add(val));
-      }
-      return acc;
-    }, new Set<string>())
-  );
-};
-
-const graph = createPackageGraph(packageInfos);
-console.log(graph);
-
-const dependencies = getAllDependencies("@latticexyz/cli", graph);
-console.log(dependencies);
-
-// path names will be in the format "packages/foo", "apps/bar" - for use in turbowatch expressions
-const depPaths = dependencies.map((dep) => {
-  const pkgInfo = packageInfos[dep];
-  return path.dirname(pkgInfo.packageJsonPath).replace(`${workspaceRoot}/`, "");
-});
-
-console.log(depPaths);
-*/
-
-// let previousPackage = "";
 const start = Date.now();
-
 const unwatchArray: string[] = [];
 
 watch({
@@ -83,7 +37,7 @@ watch({
       interruptible: true,
       initialRun: true,
       persistent: true,
-      onChange: async ({ spawn, files, first, abortSignal }: ChangeEvent) => {
+      onChange: async ({ spawn, files, first, abortSignal, watcher }: ChangeEvent) => {
         if (first) {
           console.log("Turbowatch started in", Date.now() - start, "ms");
         } else {
@@ -98,40 +52,21 @@ watch({
         const index = filenameTokens.lastIndexOf("packages");
         const changedWorkspace = filenameTokens[index + 1];
 
-        // const onChangeTime = new Date();
-        // const timeDiff = (onChangeTime - previousTimeMap.get(changedWorkspace)) / 1000; //in ms
-        // const seconds = Math.round(timeDiff);
-
-        const chokidar = files[0].chokidar;
         const packagePath = filenameTokens.slice(0, index + 2).join("/");
         console.log(packagePath);
 
-        // console.log(previousPackage, changedWorkspace, seconds);
-        // if (previousPackage != changedWorkspace || seconds > WATCH_DELAY_SECONDS) {
-        //   previousTimeMap.set(changedWorkspace, Date.now());
-        //   previousPackage = changedWorkspace;
-
-        // await spawn`turbo build --filter=${changedWorkspace}^...`;
-
-        chokidar?.unwatch(packagePath);
+        watcher?.unwatch?.(packagePath);
         unwatchArray.push(packagePath);
         console.log("unwatch " + packagePath);
 
         await spawn`turbo build --filter=${changedWorkspace}`.then(() => {
           unwatchArray.forEach((element) => {
-            chokidar?.add(element);
+            watcher?.add?.(element);
           });
           console.log("watch " + packagePath);
         });
 
-        // }
-
-        // await spawn`turbo build --filter=store`;
         if (abortSignal?.aborted) return;
-
-        // start the workspace watch script
-        // console.log("Starting watch task");
-        // await spawn`turbo run dev --only --filter ${targetWorkspace}`;
       },
     },
   ],
